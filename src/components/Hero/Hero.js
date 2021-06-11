521e924ec905361121493381839fdf5bf7010e31
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-06-11 20:36:41 +0100

    Completed header section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To My
+        <br />
+        Portfolio Website
+      </SectionTitle>
+      <SectionText>
+        I am a Frontend Web Developer specialising in JavaScript web
+        applications
+      </SectionText>
+      <Button onClick={() => (window.location = 'https://dev.to/narhlawson')}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
